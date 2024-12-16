55fd78018d657f8c16d7663eb411c98625e9703a
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-12-16 15:16:45 -0600

    Sorted the array for problem 4

--- AIE_04/AIE_04.cpp
+++ AIE_04/AIE_04.cpp
@@ -37,13 +37,20 @@ void SortArray(int* arr, int count)
 	// TODO: 
 	// modify the values in the array so that when this method returns
 	// all values are sorted in ascending order
+
+
 	for (int i = 0; i < count - 1; i++)
 	{
-		for (int j = 0; j < count - i - 1; i++)
+		for (int j = 0; j < count - i - 1; j++)
 		{
-			if (count == -1)
+			if (arr[j] > arr[j + 1])
 			{
-				std::cout << count << std::endl;
+				// making a temporary array equal to one of the values
+				int tempArray = arr[j];
+
+				// Make arr[j] equal to the other value
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tempArray;
 			}
 		}
 	}
